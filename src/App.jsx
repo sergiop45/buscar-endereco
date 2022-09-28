629a720c6dcc6e7742fc3e5bdf92e7f18345513f
sergiop45 <sergiopsantos33@gmail.com>
2022-09-28 20:55:57 -0300

    buscador-cep

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Buscador } from './components/buscador'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Buscador />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/buscador/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useState } from 'react';
+import './style.css';
+
+
+
+
+export const Buscador = () => {
+const [cep, setCep] = useState("");
+const [endereco, setEndereco] = useState([]);
+
+const getCep = async(e, numero) => {
+    
+    e.preventDefault();
+    let url = "https://viacep.com.br/ws/"+numero+"/json/";
+    setEndereco(false);
+    await fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        setEndereco(data)
+        console.log(data)
+    }).catch((err) => console.log("erro fetch -> "+err));
+   
+}    
+
+  return (
+    
+    <div>
+
+        <form action="" className='form' onSubmit={(e) => getCep(e, cep)}>
+            <div className='input'>
+                Cep: <input type="text" name='cep' onChange={(e) => setCep(e.target.value)}/>
+            </div>
+            <button>Buscar</button>
+            <br />
+            
+            {endereco.cep ? 
+            <div className="dados">
+                <h3>Endereco:</h3>
+                <p>Cep: {endereco.cep} </p>
+                <p>Rua: {endereco.logradouro} </p>
+                <p>Bairro: {endereco.bairro} </p>
+                <p>Cidade: {endereco.localidade} </p>
+                <p>Estado: {endereco.uf} </p>
+                {endereco.complemento ? <p>Complemento: {endereco.complemento} </p> : <br/>}
+            </div> : <p>Digite um cep valido e pressione o butão</p> }
+            
+            
+
+        </form>
+    </div>
+  )
+}
